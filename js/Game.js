b4b1044e0e699eb2a38e1bbefca3967555abbb0d
joshuad123 <djoshua5265@gmail.com>
2020-08-27 16:49:10 +0530

    added images

--- js/Game.js
+++ js/Game.js
@@ -29,11 +29,15 @@ class Game {
       form.display();
     }
 
-    car1 = createSprite(100,200);
-    car2 = createSprite(300,200);
-    car3 = createSprite(500,200);
-    car4 = createSprite(700,200);
+    car1 = createSprite(375,200);
+    car2 = createSprite(575,200);
+    car3 = createSprite(775,200);
+    car4 = createSprite(975,200);
     cars = [car1, car2, car3, car4];
+    car1.addImage(car1i);
+    car2.addImage(car2i);
+    car3.addImage(car3i);
+    car4.addImage(car4i);
   }
 
   play(){
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("white")
+      image(track, 0, -displayHeight*4, displayWidth, displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -56,18 +62,22 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        //x = x + 200;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
+        //cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
+        
+
+        if(keyIsDown(LEFT_ARROW) && player.index !== null){
+          cars[index-1].x=cars [index-1].x-10
         }
-       
+      }
         //textSize(15);
         //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
@@ -77,8 +87,20 @@ class Game {
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
       player.update();
+      console.log(player.distance)
+    }
+
+
+
+    if (player.distance>3660){
+      gameState=2
     }
 
     drawSprites();
+  }
+
+  end(){
+     console.log("game over");
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1i,car2i,car3i,car4i,track;
 
+function preload(){
+  car1i=loadImage ("images/car1.png")
+  car2i=loadImage ("images/car2.png")
+  car3i=loadImage ("images/car3.png")
+  car4i=loadImage ("images/car4.png")
+  track=loadImage("images/track.jpg")
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -28,4 +36,9 @@ function draw(){
     clear();
     game.play();
   }
+
+  if(gameState=== 2){
+    game.end();
+  }
 }
+
